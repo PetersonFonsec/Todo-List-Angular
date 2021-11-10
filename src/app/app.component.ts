f4c6694f35bc4cd8a799ba26cca0020132cfd632
Peterson <petersonsimiao1@gmail.com>
2021-11-10 00:16:36 -0300

    feat: implementado a logica de adicionar novas tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type Task = {
+  title: string;
+  isComplet: boolean;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [];
+  newTask = '';
+
+  addTask(): void {
+    if (this.hasTask()) return this.clearInput();
+
+    this.tasks.push({
+      title: this.newTask,
+      isComplet: false,
+    });
+
+    this.clearInput();
+  }
+
+  hasTask(): boolean {
+    return this.tasks.map((task) => task.title).includes(this.newTask);
+  }
+
+  clearInput(): void {
+    this.newTask = '';
+  }
 }
